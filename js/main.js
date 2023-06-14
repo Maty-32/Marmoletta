c2ef94697cd19d225626919fde0f120f9ade0337
Matias Pucheta <Pucheta32maty@gmail.com>
2023-06-14 17:13:06 -0300

    Carrousel automatico y con los punto smas lindos

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
 const contenedor = document.querySelector(".contenedor");
 const punto = document.querySelectorAll(".punto");
-let counter = 0
+let counter = 0;
 
 punto.forEach((cadaPunto, i) => {
-    punto[i].addEventListener(`click`, () => {
-        let posicion = i;
-        let operacion = posicion * -20;
+  punto[i].addEventListener(`click`, () => {
+    let posicion = i;
+    let operacion = posicion * -20;
 
-        contenedor.style.transform = `translateX(${operacion}%)`;
+    contenedor.style.transform = `translateX(${operacion}%)`;
 
-        punto.forEach((cadaPunto, i) => {
-            punto[i].classList.remove(`activo`);
-        });
-        punto[i].classList.add(`activo`);
+    punto.forEach((cadaPunto, i) => {
+      punto[i].classList.remove(`activo`);
     });
+    punto[i].classList.add(`activo`);
+  });
 });
 
+let index = 1
+setInterval(() => {
+   punto[index].click() 
+   index++
+   if (index==5) {
+    index = 0
+   }
+}, 5000);
 
 
-
-
-
-
+    
